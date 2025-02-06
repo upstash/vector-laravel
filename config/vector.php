65ea6fa7641032971d983094d854a76488748836
Jorge Lapa <2780099+heyjorgedev@users.noreply.github.com>
2025-02-07 03:29:33 +0700

    add description to config

--- config/vector.php
+++ config/vector.php
@@ -1,12 +1,20 @@
 <?php
 
 return [
+
+    /**
+     * The default connection name to use for the vector client.
+     */
     'default' => env('UPSTASH_VECTOR_CONNECTION', 'default'),
 
+    /**
+     * The list of connections to use for the vector client.
+     */
     'connections' => [
         'default' => [
             'url' => env('UPSTASH_VECTOR_REST_URL'),
             'token' => env('UPSTASH_VECTOR_REST_TOKEN'),
         ],
     ],
+
 ];
